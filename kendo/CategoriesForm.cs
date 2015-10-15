8b9d40aa269295211fb3f9d3331c2b6437155852
RoxanaParau <roxana.parau@xivic.com>
2015-10-15 15:26:19 +0300

    part model

--- kendo/CategoriesForm.cs
+++ kendo/CategoriesForm.cs
@@ -15,93 +15,45 @@ namespace kendo
         public CategoriesForm()
         {
             InitializeComponent();
-        }
-
-        private DataGridView dgv = new DataGridView();
-
+        }   
+        
+        private List<ParticipantModel> participantsList = new List<ParticipantModel>(); 
         public CategoriesForm(DataGridView dgv)
         {
             InitializeComponent();
-            this.dgv = dgv;
-
+            try{
+            for (int i = 0; i < dgv.Rows.Count - 1; i++)
+            {
+                participantsList.Add(new ParticipantModel
+                {
+                    id =int.Parse( dgv.Rows[i].Cells["number"].Value.ToString()),
+                    name = dgv.Rows[i].Cells["nume"].Value.ToString(),
+                    sex = dgv.Rows[i].Cells["SEX"].Value.ToString(),
+                    varsta = int.Parse(dgv.Rows[i].Cells["Ani"].Value.ToString())
+                });
+            }
+            }catch(Exception e){
+                    MessageBox.Show("O eroarea a intervenit.Va rugam verificati formatul fisierului excel.");
+                }
             showCategories();
         }
 
         private void showCategories()
         {
-
-            dgv.Sort(dgv.Columns["SEX"], ListSortDirection.Ascending);
-            for (int i = 0; i < dgv.Columns.Count; i++)
+            participantsList.OrderBy(p => p.sex).ThenBy(p => p.varsta);
+            int categ = 1;
+            for (int i = 0; i < participantsList.Count; i++)
             {
-
-                dgvCategories.Columns.Add(new DataGridViewColumn(dgv.Columns[i].CellTemplate));
-            }
-            DgvCategoriesOnSortCompare();
-            //  dgvCategories.Sort(dgvCategories.Columns["SEX"], ListSortDirection.Ascending);
-
-        }
-
-        private void DgvCategoriesOnSortCompare()
-        {
-            int j = 0;
-            for (int i = 0; i < dgv.Rows.Count - 1; i++)
-            {
-                for (int k = i + 1; k < dgv.Rows.Count; k++)
+                participantsList[i].categorie = categ;
+                if (i % 5 == 0)
                 {
-                    var nr1 =(dgv.Rows[i].Cells["Ani"].Value!=null&& !string.IsNullOrEmpty( dgv.Rows[i].Cells["Ani"].Value.ToString() )) ? int.Parse(dgv.Rows[i].Cells["Ani"].Value.ToString()) : 0;
-                    var nr2 =(dgv.Rows[k].Cells["Ani"].Value!=null&&!string.IsNullOrEmpty( dgv.Rows[k].Cells["Ani"].Value.ToString()))? int.Parse(dgv.Rows[k].Cells["Ani"].Value.ToString()) : 0;
-                    if (nr1 > nr2)
-                    {
-                        var r2 = dgv.Rows[i];
-                        var r1 = dgv.Rows[k];
-                        int c = 0;
-                        foreach (DataGridViewCell cell in r1.Cells)
-                        {
-                            dgvCategories.Rows.Add(new DataGridViewRow());
-
-                            dgvCategories.Rows[j].Cells[c].Value = cell.Value;
-
-                            c++;
-                        }
-                        j++;
-                        c = 0;
-                        foreach (DataGridViewCell cell in r2.Cells)
-                        {
-                            dgvCategories.Rows.Add(new DataGridViewRow());
-                            dgvCategories.Rows[j].Cells[c].Value = cell.Value;
-                            c++;
-                        }
-                        j++;
-
-                    }
-                    else
-                    {
-                        var r1 = dgv.Rows[i];
-                        var r2 = dgv.Rows[k];
-                        int c = 0;
-                        foreach (DataGridViewCell cell in r1.Cells)
-                        {
-                            dgvCategories.Rows.Add(new DataGridViewRow());
-
-                            dgvCategories.Rows[j].Cells[c].Value = cell.Value;
-
-                            c++;
-                        }
-                        j++;
-                        c = 0;
-                        //foreach (DataGridViewCell cell in r2.Cells)
-                        //{
-                        //    dgvCategories.Rows.Add(new DataGridViewRow());
-
-                        //    dgvCategories.Rows[j].Cells[c].Value = cell.Value;
-
-                        //    c++;
-                        //}
-                        //  j++;
-                    }
+                    participantsList[i].categorie = categ;
+                    categ++;
                 }
-
             }
+            dgvCategories.DataSource = participantsList;   
+
         }
+
     }
 }
---
+++ kendo/ParticipantModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kendo
+{
+    class ParticipantModel
+    {
+       public int id { get; set; }
+       public string name { get; set; }
+       public int varsta { get; set; }
+       public string sex { get; set; }
+       public int categorie { get; set; }
+    }
+}
